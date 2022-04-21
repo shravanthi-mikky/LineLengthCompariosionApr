ba933298829acb705fba9fa3502551825600eb2c
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-21 16:55:02 +0530

    Comparing lenthgs Using equals Method!

--- LineLength/Program.cs
+++ LineLength/Program.cs
@@ -16,3 +16,20 @@ static double Distance(double x1, double x2, double y1, double y2)
     var result = Math.Sqrt(temp1 + temp2);
     return result;
 }
+Console.WriteLine("Please Enter p and q coordinates ");
+var p = Convert.ToDouble(Console.ReadLine());
+var q = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please Enter x2 and y2 coordinates ");
+var r = Convert.ToDouble(Console.ReadLine());
+var s = Convert.ToDouble(Console.ReadLine());
+var finalResult2 = Distance(p, r, q, s);
+Console.WriteLine("Distance between {0},{1} and {2},{3} is {4:F}", p, q, r, s, finalResult2);
+static void ComparisionByEquals(double finalResult, double finalResult2)
+{
+    bool status = finalResult.Equals(finalResult2);
+    if (status)
+        Console.WriteLine("{0} is equal to {1}", finalResult, finalResult2);
+    else
+        Console.WriteLine("{0} is not equal to {1}", finalResult, finalResult2);
+}
+ComparisionByEquals(finalResult,finalResult2);
