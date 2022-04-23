1cb2a178564fa24fa2820a0c53c4d8219c404298
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-23 17:15:28 +0530

    Using Class and methods

---
+++ LineLength/CompareTo_uc3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineLength
+{
+    public class CompareTo_uc3
+    {
+        public void CompareToMethod(double finalResult, double finalResult2)
+        {
+            int res = finalResult.CompareTo(finalResult2);
+            Console.WriteLine(res);
+            if (res == 0)
+            {
+                Console.WriteLine("Lengths are equal!");
+            }
+            if (res >= 0)
+            {
+                Console.WriteLine("Line 1 is greator than line 2 ");
+            }
+            else
+            {
+                Console.WriteLine("Line 2 is greator than line 1 ");
+            }
+        }
+    }
+}
